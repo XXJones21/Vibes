770aad85ade898af04dc9df2dd1e459f178a97a8
Joshua Jones <jones@Joshuas-MacBook-Air.local>
2024-12-04 19:24:39 -0500

    fix: restore library request for recently played and apply spatial audio filtering

--- Packages/MusicService/Sources/MusicService/MusicService+Albums.swift
+++ Packages/MusicService/Sources/MusicService/MusicService+Albums.swift
@@ -32,106 +32,16 @@ extension VibesMusicService {
         }
     }
     
-    /// Fetches recently played albums
-    internal func fetchRecentlyPlayed() async throws -> [Album] {
-        var request = MusicCatalogSearchRequest(term: "dolby atmos", types: [Album.self])
-        request.limit = Constants.pageSize
-        let response = try await request.response()
-        return filterSpatialAudio(Array(response.albums))
-    }
-    
-    /// Fetches top charting albums
-    internal func fetchTopCharts() async throws -> [Album] {
-        var allAlbums: [Album] = []
-        let requestsNeeded = (Constants.albumsPerRow + Constants.pageSize - 1) / Constants.pageSize
-        let maxOffset = requestsNeeded * Constants.pageSize
-        
-        for offset in stride(from: 0, to: maxOffset, by: Constants.pageSize) {
-            var request = MusicCatalogSearchRequest(term: "top hits", types: [Album.self])
-            request.limit = Constants.pageSize
-            request.offset = offset
-            let response = try await request.response()
-            allAlbums.append(contentsOf: response.albums)
-            
-            if allAlbums.count >= Constants.albumsPerRow {
-                break
-            }
-        }
-        return filterSpatialAudio(allAlbums)
-    }
-    
-    /// Fetches new releases
-    internal func fetchNewReleases() async throws -> [Album] {
-        var allAlbums: [Album] = []
-        let requestsNeeded = (Constants.albumsPerRow + Constants.pageSize - 1) / Constants.pageSize
-        let maxOffset = requestsNeeded * Constants.pageSize
-        
-        for offset in stride(from: 0, to: maxOffset, by: Constants.pageSize) {
-            var request = MusicCatalogSearchRequest(term: "new releases", types: [Album.self])
-            request.limit = Constants.pageSize
-            request.offset = offset
-            let response = try await request.response()
-            allAlbums.append(contentsOf: response.albums)
-            
-            if allAlbums.count >= Constants.albumsPerRow {
-                break
-            }
-        }
-        return filterSpatialAudio(allAlbums)
-    }
-    
-    /// Fetches spatial audio albums
-    internal func fetchSpatialAudioAlbums() async throws -> [Album] {
-        var allAlbums: [Album] = []
-        let requestsNeeded = (Constants.albumsPerRow + Constants.pageSize - 1) / Constants.pageSize
-        let maxOffset = requestsNeeded * Constants.pageSize
-        
-        // Try multiple search terms to get better results
-        let searchTerms = ["dolby atmos", "spatial audio", "360 audio"]
-        
-        for term in searchTerms {
-            for offset in stride(from: 0, to: maxOffset, by: Constants.pageSize) {
-                var request = MusicCatalogSearchRequest(term: term, types: [Album.self])
-                request.limit = Constants.pageSize
-                request.offset = offset
-                let response = try await request.response()
-                allAlbums.append(contentsOf: response.albums)
-                
-                if allAlbums.count >= Constants.albumsPerRow * 2 {  // Get more to filter
-                    break
-                }
-            }
-        }
-        
-        // Remove duplicates
-        let uniqueAlbums = Array(Set(allAlbums))
-        return filterSpatialAudio(uniqueAlbums)
-    }
-    
-    /// Fetches editor's picks
-    internal func fetchEditorsPicks() async throws -> [Album] {
-        var allAlbums: [Album] = []
-        let requestsNeeded = (Constants.albumsPerRow + Constants.pageSize - 1) / Constants.pageSize
-        let maxOffset = requestsNeeded * Constants.pageSize
-        
-        for offset in stride(from: 0, to: maxOffset, by: Constants.pageSize) {
-            var request = MusicCatalogSearchRequest(term: "featured hits", types: [Album.self])
-            request.limit = Constants.pageSize
-            request.offset = offset
-            let response = try await request.response()
-            allAlbums.append(contentsOf: response.albums)
-            
-            if allAlbums.count >= Constants.albumsPerRow {
-                break
-            }
-        }
-        return filterSpatialAudio(allAlbums)
-    }
-    
     /// Helper function to filter for spatial audio support
     private func filterSpatialAudio(_ albums: [Album]) -> [Album] {
         // First get all albums that definitely support Dolby Atmos
         let spatialAlbums = albums.filter { album in
+            // Skip singles
+            guard !(album.title.localizedCaseInsensitiveContains("single") ||
+                   album.title.localizedCaseInsensitiveContains(" - single")) else {
+                return false
+            }
+            
             // Check audio variants first
             if let variants = album.audioVariants {
                 return variants.contains(where: { variant in
@@ -160,6 +70,134 @@ extension VibesMusicService {
         
         // Otherwise, return the original albums (up to albumsPerRow)
         return Array(albums.prefix(Constants.albumsPerRow))
+    }
+    
+    /// Fetches recently played albums
+    @available(visionOS 2.0, *)
+    internal func fetchRecentlyPlayed() async throws -> [Album] {
+        var request = MusicLibraryRequest<Album>()
+        request.limit = Constants.pageSize * 2  // Get more items to filter from
+        let response = try await request.response()
+        let items = Array(response.items)
+        
+        // Apply spatial audio filtering to library items
+        return filterSpatialAudio(items)
+    }
+    
+    /// Fetches top charting albums
+    internal func fetchTopCharts() async throws -> [Album] {
+        var allAlbums: [Album] = []
+        let requestsNeeded = (Constants.albumsPerRow + Constants.pageSize - 1) / Constants.pageSize
+        let maxOffset = requestsNeeded * Constants.pageSize
+        
+        for offset in stride(from: 0, to: maxOffset, by: Constants.pageSize) {
+            var request = MusicCatalogSearchRequest(term: "top hits", types: [Album.self])
+            request.limit = Constants.pageSize
+            request.offset = offset
+            let response = try await request.response()
+            
+            // Filter out singles
+            let fullAlbums = response.albums.filter { album in
+                !(album.title.localizedCaseInsensitiveContains("single") ||
+                  album.title.localizedCaseInsensitiveContains(" - single"))
+            }
+            
+            allAlbums.append(contentsOf: fullAlbums)
+            
+            if allAlbums.count >= Constants.albumsPerRow {
+                break
+            }
+        }
+        return filterSpatialAudio(allAlbums)
+    }
+    
+    /// Fetches new releases
+    internal func fetchNewReleases() async throws -> [Album] {
+        var allAlbums: [Album] = []
+        let requestsNeeded = (Constants.albumsPerRow + Constants.pageSize - 1) / Constants.pageSize
+        let maxOffset = requestsNeeded * Constants.pageSize
+        
+        for offset in stride(from: 0, to: maxOffset, by: Constants.pageSize) {
+            var request = MusicCatalogSearchRequest(term: "new releases", types: [Album.self])
+            request.limit = Constants.pageSize
+            request.offset = offset
+            let response = try await request.response()
+            
+            // Filter out singles
+            let fullAlbums = response.albums.filter { album in
+                !(album.title.localizedCaseInsensitiveContains("single") ||
+                  album.title.localizedCaseInsensitiveContains(" - single"))
+            }
+            
+            allAlbums.append(contentsOf: fullAlbums)
+            
+            if allAlbums.count >= Constants.albumsPerRow {
+                break
+            }
+        }
+        return filterSpatialAudio(allAlbums)
+    }
+    
+    /// Fetches spatial audio albums
+    internal func fetchSpatialAudioAlbums() async throws -> [Album] {
+        var allAlbums: [Album] = []
+        let requestsNeeded = (Constants.albumsPerRow + Constants.pageSize - 1) / Constants.pageSize
+        let maxOffset = requestsNeeded * Constants.pageSize
+        
+        // Try multiple search terms to get better results
+        let searchTerms = ["dolby atmos", "spatial audio", "360 audio"]
+        
+        for term in searchTerms {
+            for offset in stride(from: 0, to: maxOffset, by: Constants.pageSize) {
+                var request = MusicCatalogSearchRequest(term: term, types: [Album.self])
+                request.limit = Constants.pageSize
+                request.offset = offset
+                let response = try await request.response()
+                
+                // Filter out singles
+                let fullAlbums = response.albums.filter { album in
+                    !(album.title.localizedCaseInsensitiveContains("single") ||
+                      album.title.localizedCaseInsensitiveContains(" - single"))
+                }
+                
+                allAlbums.append(contentsOf: fullAlbums)
+                
+                if allAlbums.count >= Constants.albumsPerRow * 2 {  // Get more to filter
+                    break
+                }
+            }
+        }
+        
+        // Remove duplicates
+        let uniqueAlbums = Array(Set(allAlbums))
+        return filterSpatialAudio(uniqueAlbums)
+    }
+    
+    /// Fetches editor's picks
+    internal func fetchEditorsPicks() async throws -> [Album] {
+        var allAlbums: [Album] = []
+        let requestsNeeded = (Constants.albumsPerRow + Constants.pageSize - 1) / Constants.pageSize
+        let maxOffset = requestsNeeded * Constants.pageSize
+        
+        for offset in stride(from: 0, to: maxOffset, by: Constants.pageSize) {
+            var request = MusicCatalogSearchRequest(term: "featured hits", types: [Album.self])
+            request.limit = Constants.pageSize
+            request.offset = offset
+            let response = try await request.response()
+            
+            // Filter out singles
+            let fullAlbums = response.albums.filter { album in
+                !(album.title.localizedCaseInsensitiveContains("single") ||
+                  album.title.localizedCaseInsensitiveContains(" - single"))
+            }
+            
+            allAlbums.append(contentsOf: fullAlbums)
+            
+            if allAlbums.count >= Constants.albumsPerRow {
+                break
+            }
+        }
+        return filterSpatialAudio(allAlbums)
     }
     
     public func fetchArtwork(for album: AlbumRepresentable, width: Int, height: Int) async throws -> Image? {
